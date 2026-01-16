e7295b4668d018d6dfb4d5b798e61142f500f2b6
vishva2069 <252988544+vishva2069@users.noreply.github.com>
2026-01-16 07:48:41 +0000

    ..

--- 01_statement.java
+++ 01_statement.java
@@ -1,5 +1,32 @@
-int age = 18;
+// User ની age input લો.
+// જો age 18 કે વધારે હોય તો "Eligible for vote" print કરો.     
+     
+int age = 20;
 
-if(age >= 20){
+if(age >= 18){
     system.out.println("Eligible for vote")
-    }
+    };
+
+// એક number લો.
+// જો number positive હોય તો "Positive Number" print કરો.
+
+int num = 30;
+
+if(num > 0)
+{
+    system.out.println("positive number");
+};
+
+(30>0) -> positive number
+(-30>0) -> negative number
+
+// એક integer લો.
+// જો number even હોય તો "Even Number" print કરો.
+
+int count = 7;
+
+if(num 2 %=== 0){
+    system.out.println("even number");
+} else{
+    system.out.println("odd number");
+}
